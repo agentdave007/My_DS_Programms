935e0009efcb9b84d4ad1d51584d562578a89560
agentdave007 <54462861+agentdave007@users.noreply.github.com>
2021-04-24 23:21:46 +0530

    Update Test.R

--- Test.R
+++ Test.R
@@ -20,3 +20,25 @@ g=function(a){a*x}
 
 f(4)
 
+
+
+x=seq(-3,2,length.out=6)
+print(x)
+y=c(-2,1,NA)
+print(2*x)
+print(y)
+z=2*x-y+1
+print(z)
+pmax(x,z)
+z[z<0]=0
+print(z)
+print(z[(2:4)])
+print((2:4))
+print(-(2:4))
+print(z[-(2:4)])
+p=0/z
+is.nan(p)
+paste(c("DU"),c("BSC"),101:104,sep="/")
+as.logical(c(0,1,"a","1","False"))
+sample(c("H","T"),size=10,replace = TRUE,prob = c(0.7,0.3))
+rep(c('X','Y','Z'),each=3)
